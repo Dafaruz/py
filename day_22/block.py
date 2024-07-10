affaeba6c0a517da3ecdda3ccca244fbea4a7a3b
Dafaruz <daniel.faruz@intel.com>
2024-07-10 21:09:45 +0300

    dan

--- day_22/block.py
+++ day_22/block.py
@@ -1,4 +1,23 @@
 from turtle import Turtle
 
+SPEED = 20
+SIZE = 5
 
-class block
+class Block (Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.step = SPEED
+        self.block_list=[]
+        self.block_creation()
+
+
+    def block_creation(self):
+
+        for index in range (0,SIZE):
+            blocks = Turtle("square")
+            blocks.size(20)
+            blocks.color("white")
+            blocks.penup()
+            blocks.speed('slow')
+            blocks.goto(x=-280 , y=(-40 + index*20))
