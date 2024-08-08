92857a82825628f0d479bf372cf0c070b82de729
Dafaruz <daniel.faruz@intel.com>
2024-08-08 22:39:42 +0300

    day 26

---
+++ day_26/day_25_csv_practice/main.py
@@ -0,0 +1,57 @@
+with open("weather_data.csv") as data_file:
+     data = data_file.read()
+     print(data)
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#
+#     print(temperature)
+
+
+import pandas
+
+
+#data = pandas.read_csv("weather_data.csv") #use pandas to read csv        #cmd to read the csv file
+
+# # print(data)
+# temp =data.temp    # get series of the temp
+# dict =data.to_dict()  # transfer data to dictionary
+# print(dict)
+#
+# list=data.temp.to_list()
+# print( sum(list)//len(list))
+#
+# z=data.temp.max()
+# print(z)
+# print(data[data.temp == data.temp.max()])
+#Get data in row:
+# monday_temp=data[data.day == "Monday"]
+#
+# fer=monday_temp.temp[0]*9/5 +32 #transfer to fer
+# print(fer)
+
+#Some refresh on dic and key values :
+
+# data_dict ={
+#      "student": ["daniel", "david", "nastia", ],
+#      "scores": [75,80,90]
+# }
+# for key in data_dict["student"]:
+#     data_dict["student"].index(key)
+#     print(f"the score of {key} is : {data_dict["scores"][data_dict["student"].index(key)]}")
+
+
+#data = pandas.DataFrame(data_dict)
+#data.to_csv("new_data.csv")
+#print(data)
+
+
+
+
+
+
---
+++ day_26/day_25_quiz/state_text.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+import pandas
+FONT = ("Courier", 24, "normal")
+
+
+class StateText (Turtle):
+
+    def __init__(self,user_guess, x_cord, y_cord):
+        super().__init__()
+        self.guess = user_guess
+        self.hideturtle()
+        self.color('black')
+        self.penup()
+        self.user_geuss_right(x_cord,y_cord)
+
+
+    def user_geuss_right (self,x_cord,y_cord):
+        self.goto(x=x_cord, y=y_cord)
+        self.write(arg=self.guess, move=False, align='center', font=('Arial', 14, 'bold'))
+
+    def game_over(self):
+        self.clear()
+        self.goto(x=0, y=0)
+        self.write(arg=f"game over", move=False, align='center', font=('Arial', 14, 'bold'))
