063270cdc597f09b992977a3c51efae35fece69e
Dafaruz <daniel.faruz@intel.com>
2024-08-13 17:35:28 +0300

    day 30 done

--- day_30/json_l/main.py
+++ day_30/json_l/main.py
@@ -1,7 +1,15 @@
+##########################################################################
+#                       This code is for a json file study               #
+#                        day 30 of code via Udamy                        #
+#                        by: Daniel Faruz                                #
+#                                                                        #
+##########################################################################
+
 from tkinter import *
 from random import choice, shuffle
 import pyperclip
-
+import json
+from tkinter import messagebox
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
            'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
            'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -12,45 +20,65 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 txt = ['', '', '']
 
+j_dict = {}
+
+
+# ---------------------------- Search DATA ------------------------------- #
+def search_data():
+    web_name = website_entry.get()
+    with open("User Data.json", "r") as json_file:
+        j_data = json.load(json_file)
+        try:
+            messagebox.showinfo(title='User Name AND Password', message=f"""the password :{j_data[web_name]["password"]}
+            \n the email: {j_data[web_name]["email"]}""")
+
+        except KeyError:
+            messagebox.showinfo(title='User Name AND Password', message=f" no data on this site")
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_pass():
 
     password = ""                # init the password
-    for letter in range(0, 6):
+    for letter in range(0, 6):          # 6 letters
         password += choice(letters)
 
-    for symbol in range(0, 3):
+    for symbol in range(0, 3):         # 3 symbols
         password += choice(symbols)
 
-    for number in range(0, 4):
+    for number in range(0, 4):         # 4 numbers
         password += choice(numbers)
 
     password_list = list(password)      # concert to list so we can shuffle
     shuffle(password_list)                     # shuffle
     password = ''.join(password_list)         # join from list to string
-    password_entry.delete(1, END)
-    password_entry.insert(END, string=password)
-    pyperclip.copy(password)
+    password_entry.delete(1, END)      # delete the entry data on the window
+    password_entry.insert(0, string=password)     # enter the new string
+    pyperclip.copy(password)                       # copy to clipboard the password
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_info():
-
-    with open("User Data.txt", mode='a') as file:     # notice mode  is appended to add new line
-
-        txt[0] = website_entry.get()
+    global j_dict
+    with open("User Data.json", mode='w') as data:     # notice mode  is appended to add new line
+        txt[0]=password_entry.get()
         txt[1] = mail_entry.get()
-        txt[2] = password_entry.get()
-        print(txt[0])
-        if '' in txt:
+        txt[2]= website_entry.get()
+        # a part for empty info
+        if "" in txt:
+            print(j_dict)
             popup = Tk()
             popup.minsize(width=300, height=200)
-            popup.title(' dont leave it empty you asshole')
+            popup.title(' one or more of the detail is empty pls refill')
+            popup.config(bg="white")
             pop_label = Label(popup, text='pls fix it u jackass', font=("Ariel", 15))
             pop_label.pack()
+
+
         else:
-            file.write(str(f"{txt[0]} | {txt[1]} |{txt[2]} \n"))
+                print(j_dict)
+                j_dict[str(website_entry.get())] = {"password": password_entry.get(), "email": mail_entry.get()}
+                json.dump(j_dict, data, indent=4)   # if statement won't do  we will dump the info
 
 # ---------------------------- UI SETUP ------------------------------------------------------------------------ #
 
@@ -71,8 +99,11 @@ logo.grid(column=1, row=0)                  # grid alignment
 generate = Button(text="Generate password", command=generate_pass)      # create the generate button
 generate.grid(column=1, row=4, sticky='e')          # notice the sticky option will stick it to west or est etc
 
-add = Button(text="Add", width=30, command=add_info)      # add to notepad
-add.grid(column=1, row=5, sticky="w", pady=5)          # grid alignment
+add = Button(text="Add", width=40, command=add_info)      # add to notepad
+add.grid(column=1, row=5, pady=5)          # grid alignment
+
+website_b = Button(text="Search", command=search_data)
+website_b.grid(column=1, row=2, sticky='e')
 
 # _____________________________________ Entry sector _____________________________________________________________#
 
@@ -80,8 +111,8 @@ add.grid(column=1, row=5, sticky="w", pady=5)          # grid alignment
 password_entry = Entry(width=30)                    # create the entry pass section
 password_entry.grid(column=1, row=4, sticky='w')       # notice the sticky option will stick it to west or est etc
 
-website_entry = Entry(width=50)                     # create the entry website section
-website_entry.grid(column=1, row=2)                 # grid alignment
+website_entry = Entry(width=40)                     # create the entry website section
+website_entry.grid(column=1, row=2, sticky='w')                 # grid alignment
 
 mail_entry = Entry(width=50)                     # create the entry user_name section
 mail_entry.grid(column=1, row=3)                   # grid alignment
